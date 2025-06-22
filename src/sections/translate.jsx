327d18571899d663219d59539306cac74efcb965
Richard Su <suruiquan10@163.com>
2025-06-22 09:28:19 -0400

    I'm so done

--- src/sections/translate.jsx
+++ src/sections/translate.jsx
@@ -22,7 +22,7 @@ export default function Translate(){
     setIsLoading(true);
     try {
       const API_URL = process.env.NODE_ENV === 'production' 
-        ? 'https://goblinator-nine.vercel.app'
+        ? 'https://translator-api.vercel.app'
         : 'http://localhost:3001';
 
       
