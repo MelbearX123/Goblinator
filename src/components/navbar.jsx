b20503b5e882d6ccc5f37356c7b1ade3cbeb8d41
amcli <andrew2024li@gmail.com>
2025-06-21 23:31:09 -0400

    updated visuals

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,12 @@
 export default function NavBar(){
   return(
-    <nav className="bg-white shadow px-6 py-4 flex justify-between items-center">
-      <a className="text-xl font-semibold text-shadow-xs">
+    <nav className="sticky top-0 bg-black shadow px-6 py-8 flex justify-between items-center">
+      <a className="text-3xl text-white font-bold text-shadow-xs">
         Goblinator
       </a>
+      <ul className="flex gap-4 sm:gap-6 text-sm sm:text-base text-gray-300 font-medium">
+        <li className="text-white text-0.5xl">Made by Silverhand</li>
+      </ul>
     </nav>
   );
 }
--- src/sections/translate.jsx
+++ src/sections/translate.jsx
@@ -16,13 +16,13 @@ export default function Translate(){
   };
 
   return(
-    <main className="flex flex-col items-center py-8 bg-gray-100 min-h-screen">
+    <main className="flex flex-col items-center py-32 bg-[#1e1e1e]">
       <div className="flex gap-4 items-center mb-6">
-        <p className="px-4 py-2 rounded-lg min-w-[90px] text-center bg-white shadow">{fromLang}</p>
-        <button className="text-2xl px-2 border-none focus:outline-none transition-transform duration-75 ease-in-out transform hover:scale-110 active:scale-125" onClick={swapLanguages}>
+        <p className="px-4 py-2 rounded-lg min-w-[90px] text-center bg-[#000000] shadow border-1 border-[#FF6B6B]">{fromLang}</p>
+        <button className="text-2xl text-white copx-2 border-none focus:outline-none transition-transform duration-50 ease-in-out transform hover:scale-135 active:scale-115" onClick={swapLanguages}>
           ⇄
         </button>
-        <a className="px-4 py-2 rounded-lg min-w-[90px] text-center bg-white shadow">{toLang}</a>
+        <a className="px-4 py-2 rounded-lg min-w-[90px] text-center bg-[#000000] border-1 border-[#FF6B6B] shadow">{toLang}</a>
       </div>
       
       <Textbox inputText={inputText} setInputText={setInputText} />
