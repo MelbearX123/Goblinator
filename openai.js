f512acb2d85f9ab43a595d064df7c0fa015aa4c5
Melodie X <mxiong@uwaterloo.ca>
2025-06-22 05:43:39 -0400

    Changed openai.js file

--- openai.js
+++ openai.js
@@ -1,16 +1,28 @@
 import OpenAI from "openai";
 require("dotenv").config();
 
-const openai = new OpenAI(process.env.OPENAI_API_KEY);
+const openai = new OpenAI({
+  api_key: process.env.OPENAI_API_KEY,
+});
 
-async function main() {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a brainrot to English translator. You should translate the phrase into plain English." }],
-    model: "ft:gpt-4.1-nano-2025-04-14:personal:brainrot-translate:Bl8MOsFc",
-    store: false, // Does not store conversation history
-  });
+async function translate() {
 
-  console.log(completion.choices[0]);
+  try{
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: "system", content: "You are a brainrot to English translator. You should translate the phrase into plain English." }],
+      model: "ft:gpt-4.1-nano-2025-04-14:personal:brainrot-translate:Bl8MOsFc", //Custom fine-tuned model
+      store: false, //Does not store conversation history
+    });
+    
+    //Log the results
+    console.log('Translation Result: ', completion.choices[0]);
+    return completion.choices[0];
+  }
+  catch (error) {
+    console.error('Error:', error.message);
+    throw error;
+  }
+
+
+
 }
-
-main();
